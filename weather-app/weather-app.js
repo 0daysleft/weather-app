42560e8df0da6832033a10a1420b157972a0aa8e
odaysleft <vincentarasa254@gmail.com>
2025-04-22 16:48:30 +0300

    testing the api data

--- weather-app/weather-app.js
+++ weather-app/weather-app.js
@@ -50,39 +50,52 @@ let time = new Date().getTime().toString();
 let dayMilliseconds =  86400000;
 
 //console.log(Number(time) + (dayMilliseconds));
+let api = `https://api.openweathermap.org/data/2.5/weather?q=nairobi&appid=${apiKey}`
 
-let api = `https://api.openweathermap.org/data/2.5/weather?q=kutus&dt=${time}&appid=${apiKey}`
 async function nowWeather() {
      let a = await fetch(api)
      let r = await a.json();
      //console.log(new Date(r.dt).toDateString());
-     //console.log(r)
+     console.log(r)
      //console.log(new Date(time - r.dt).toLocaleTimeString())
 
      let weather = r.weather[0].main;
      let weatherDesc = r.weather[0].description;
      let windSpeed =  r.wind.speed
      let windDeg = r.wind.deg;
-     let windgust = r.wind.windgust;
+     let windgust = r.wind.gust;
      let country = r.sys.country;
      let countrySunrise = new Date(r.sys.sunrise).toUTCString();
      let countrySunset = new Date(r.sys.sunset).toUTCString();
 
      console.log(
           "Weather: ", weather,
-          " WeatherDesc: ", weatherDesc,
-          " WindSpeed: ", windSpeed,
-          " WindDeg: ", windDeg,
-          " WindGust: ", windgust,
-          " Country: ", country, 
-          " CountrySunrise: ", countrySunrise,
-          " CountrySunSet: ", countrySunset
+          "\nWeatherDesc: ", weatherDesc,
+          "\nWindSpeed: ", windSpeed,
+          "\nWindDeg: ", windDeg,
+          "\nWindGust: ", windgust,
+          "\nCountry: ", country, 
+          "\nCountrySunrise: ", countrySunrise,
+          "\nCountrySunSet: ", countrySunset
      )
 }
 
-nowWeather();
+//nowWeather();
 
-let api2 = `https://api.openweathermap.org/data/2.5/weather?q=nairobi&appid=${apiKey}`
+//console.log(typeof Date.now())
+
+let b = Date.now() + dayMilliseconds;
+console.log(new Date(b).toLocaleTimeString());
+
+for(let i = Date.now(); i <= (Date.now() + (dayMilliseconds * 3)); i += dayMilliseconds){
+     let times = new Date(i);
+     todayWeatherTemperature.innerHTML += times + "</br>";
+     //console.log(i);
+     //console.log(new Date(i));
+}
+
+
+let api2 = `https://api.openweathermap.org/data/2.5/weather?q=kutus&dt=${time}&appid=${apiKey}`
 async function nowWeather2() {
      let a = await fetch(api2)
      let r = await a.json();
