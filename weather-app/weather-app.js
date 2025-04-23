a5688f85f53da08fd32f28c9bdc6429d503f29b2
odaysleft <vincentarasa254@gmail.com>
2025-04-23 12:14:59 +0300

    decided not use it(geo location)

--- weather-app/weather-app.js
+++ weather-app/weather-app.js
@@ -48,8 +48,6 @@ const humidityBarPercentage = document.querySelector('.humidity-bar-data');
 //https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={API key}
 //  
 
-
-
 let  a;
 let r;
 let api2;
